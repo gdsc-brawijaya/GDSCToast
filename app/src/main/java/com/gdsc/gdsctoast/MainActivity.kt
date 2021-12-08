62f6e391e95a6039c7ef3cfc1aabab7a1372fbbc
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-12-08 12:49:00 +0700

    init commit

--- app/src/main/java/com/gdsc/gdsctoast/MainActivity.kt
+++ app/src/main/java/com/gdsc/gdsctoast/MainActivity.kt
@@ -2,10 +2,16 @@ package com.gdsc.gdsctoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gdsc.gdsctoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
